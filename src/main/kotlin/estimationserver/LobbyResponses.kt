5f3ba36782bfa7c3c48ea8fa79ea0278ac55be9e
talham7391 <talham7391@hotmail.com>
2019-10-06 17:18:37 -0400

    Send type with response to client

--- src/main/kotlin/estimationserver/LobbyResponses.kt
+++ src/main/kotlin/estimationserver/LobbyResponses.kt
@@ -2,4 +2,4 @@ package estimationserver
 
 import estimationserver.party.Player
 
-data class ConnectedPlayersResponse (val players: Set<Player>)
+data class ConnectedPlayersResponse (val players: Set<Player>, val type: String = "CONNECTED_PLAYERS")
